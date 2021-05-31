360430687e2526a809e9e6deac2c358a553b140e
dileep <dileepkumar2896@gmail.com>
2021-05-31 12:04:03 +0530

    Add transfer learning create model function.

--- tensorflowHelper.py
+++ tensorflowHelper.py
@@ -1,6 +1,9 @@
 import zipfile
 import os
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow.keras import layers
 
 
 class TensorFlowHelper:
@@ -53,3 +56,32 @@ class TensorFlowHelper:
         plt.title("Accuracy")
         plt.xlabel("Epochs")
         plt.legend()
+
+    @staticmethod
+    def tl_create_model(model_url, num_classes=10, Image_shape=(224, 224)):
+        """
+        Takes a TensorFlow Hub URL and create a Keras Sequential model with it.
+        Used for transfer learning...
+        For Image classification
+
+        :param model_url: A TensorFlow Hub feature extraction URL.
+        :param num_classes: Number of output neurons in the output layer,
+                            should be equal to number of target classes, default 10.
+        :param Image_shape: Image shape for the input layer
+        :return: An un compiled Keras Sequential model with model_url as feature extractor
+                 layer and Dense output layer with num_classes output neurons.
+        """
+
+        # Download the pretrained model and save it KerasLayer
+        feature_extractor_layer = hub.KerasLayer(model_url,
+                                                 trainable=False,  # freeze the already learned patterns
+                                                 name="feature_extractor_model",
+                                                 input_shape=Image_shape + (3,))
+
+        # Create our own model
+        model = tf.keras.Sequential([
+            feature_extractor_layer,
+            layers.Dense(num_classes, activation="softmax", name="output_layer")
+        ])
+
+        return model
